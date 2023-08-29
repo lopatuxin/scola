b2d45b212dcb072f07f8faa67461f2a589d804a1
Антон <lopatuxin.a@gmail.com>
2023-08-29 20:23:06 +0300

    Вывод блоков на странице юзера

---
+++ src/main/java/ru/lopatuxin/scola/controllers/BlockController.java
@@ -0,0 +1,41 @@
+package ru.lopatuxin.scola.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.lopatuxin.scola.models.Student;
+import ru.lopatuxin.scola.services.BlockService;
+import ru.lopatuxin.scola.services.StudentService;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/block")
+public class BlockController {
+
+    private final BlockService blockService;
+    private final StudentService studentService;
+
+    @Autowired
+    public BlockController(BlockService blockService, StudentService studentService) {
+        this.blockService = blockService;
+        this.studentService = studentService;
+    }
+
+    @ModelAttribute("user")
+    public String getUser(@AuthenticationPrincipal UserDetails userDetails) {
+        Optional<Student> student = studentService.findByEmail(userDetails.getUsername());
+        return student.map(value -> value.getName() + " " + value.getSurname()).orElse("User");
+    }
+
+    @GetMapping
+    public String getBlocksPage(Model model) {
+        model.addAttribute("blocks", blockService.findAll());
+        return "block/blocks";
+    }
+}
